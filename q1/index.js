6c744df3d977f8468bc49a12f65501b73c8a5ddb
madhavth <madhavth77@gmail.com>
2022-10-23 20:20:11 -0500

    completed q1

--- q1/index.js
+++ q1/index.js
@@ -2,4 +2,35 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+
+app.set(express.static(`${__dirname}`));
+app.use('/css', express.static(path.join(`${__dirname}`,'css')));
+
 app.set('view engine', 'ejs');
+app.set('views', path.join(`${__dirname}`, "view"));
+
+const dayStyle = `body {
+   background-color: lightblue;
+   color: black;
+}`;
+
+const nightStyle = `body {
+   background-color: black;
+   color: white;
+}`;
+
+app.get('/', (req,res)=> {
+   const date = new Date();
+   const hours = date.getHours();
+
+   let isDay = hours >=6 && hours < 18;
+
+   res.render("index", {
+      time: date.toTimeString(),
+      css: isDay? dayStyle: nightStyle
+   });
+});
+
+
+app.listen(3000);
